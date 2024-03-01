1f7a21509ebae7a2ffc6217ea6dfdd4780e33ee2
muziqiu2 <2443539514@qq.com>
2024-03-01 16:10:40 +0800

    Site updated: 2024-03-01 16:10:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/1475875489/","posts/125/","posts/3145788926/","posts/1751468545/","posts/1985886148/","posts/926119936/"];function toRandomPost(){
+var posts=["posts/1475875489/","posts/125/","posts/3145788926/","posts/1751468545/","posts/926119936/","posts/1985886148/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
