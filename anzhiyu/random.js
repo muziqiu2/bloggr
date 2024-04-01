3ea3f525a2963218c164231e3e6bd8c05f99db70
muziqiu2 <2443539514@qq.com>
2024-04-01 17:06:59 +0800

    Site updated: 2024-04-01 17:06:58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/1475875489/","posts/125/","posts/3145788926/","posts/1751468545/","posts/30169/","posts/926119936/","posts/1985886148/"];function toRandomPost(){
+var posts=["posts/1475875489/","posts/3145788926/","posts/1751468545/","posts/125/","posts/926119936/","posts/1985886148/","posts/30169/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
